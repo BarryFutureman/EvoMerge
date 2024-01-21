f38045eb77d0b8d6cd09af9b2d94ebcaa080ec46
Barry <barryfutureman@gmail.com>
2024-01-21 16:39:06 -0500

    extra scripts

---
+++ runs/MistralEvolve/population/download_models.py
@@ -0,0 +1,11 @@
+from huggingface_hub import snapshot_download
+
+model_repos = ["mlabonne/NeuralBeagle14-7B",
+               "udkai/Turdus",
+               "Weyaxi/MetaMath-Chupacabra-7B-v2.01-Slerp",
+               "senseable/Westlake-7B",
+               ]
+
+for repo in model_repos:
+    folder_name = repo.split("/", 1)[-1]
+    snapshot_download(repo_id=repo, local_dir=f"./{folder_name}", local_dir_use_symlinks=False)
